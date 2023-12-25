0367b1cf5de205636b04045e0021b57760a6856e
고범석 <kobums23@gmail.com>
2023-12-25 21:09:28 +0900

    add golang

--- config/config.go
+++ config/config.go
@@ -1,58 +1,103 @@
 package config
 
 import (
-	"fmt"
-
-	"github.com/spf13/viper"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-var (
-	Database			string
-	User				string
-	Server				string
-	Password			string
-	Language			string
-	Port				string
-	UploadPath			string
-)
-
-func init() {
-	viper.SetConfigType("json")
-	viper.SetConfigName("config")
-	viper.AddConfigPath("./config")
-	viper.AddConfigPath("../config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-
-	if err != nil {
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-
-	if value := viper.Get("Database"); value != nil {
-		Database = value.(string)
-	}
-
-	if value := viper.Get("User"); value != nil {
-		User = value.(string)
-	}
-
-	if value := viper.Get("Server"); value != nil {
-		Server = value.(string)
-	}
-
-	if value := viper.Get("Password"); value != nil {
-		Password = value.(string)
-	}
-
-	if value := viper.Get("Language"); value != nil {
-		Language = value.(string)
-	}
-
-	if value := viper.Get("port"); value != nil {
-		Port = value.(string)
-	}
-
-	if value := viper.Get("uploadPath"); value != nil {
-		UploadPath = value.(string)
-	}
+type GpaMap struct {
+	Name string   `json:"name"`
+	Data []string `json:"data"`
 }
+
+type GpaJoin struct {
+	Name   string `json:"name"`
+	Column string `json:"column"`
+	Prefix string `json:"prefix"`
+}
+
+type GpaCompare struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+type SessionPair struct {
+	Key    string `json:"key"`
+	Column string `json:"column"`
+}
+
+type Gpa struct {
+	Name    string        `json:"name"`
+	Map     []GpaMap      `json:"map"`
+	Method  []string      `json:"method"`
+	Join    []GpaJoin     `json:"join"`
+	Compare []GpaCompare  `json:"compare"`
+	Session []SessionPair `json:"session"`
+}
+
+type ModelConfig struct {
+	Buildtool string `json:"buildtool"`
+	Server    string `json:"server"`
+	Database  string `json:"database"`
+	User      string `json:"user"`
+	Password  string `json:"password"`
+	Auth      string `json:"auth"`
+	Language  string `json:"language"`
+	Gpa       []Gpa  `json:"table"`
+}
+
+
+func Init(dir string) ModelConfig {
+	file, _ := os.Open(path.Join(dir, "config/config.json"))
+	defer file.Close()
+	data, _ := ioutil.ReadAll(file)
+
+	var modelConfig ModelConfig
+
+	json.Unmarshal(data, &modelConfig)
+
+	return modelConfig
+}
+
+// func Init() {
+// 	viper.SetConfigType("json")
+// 	viper.SetConfigName("config")
+// 	viper.AddConfigPath("./config")
+// 	viper.AddConfigPath("../config")
+// 	viper.AddConfigPath(".")
+// 	err := viper.ReadInConfig()
+
+// 	if err != nil {
+// 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+// 	}
+
+// 	if value := viper.Get("Database"); value != nil {
+// 		Database = value.(string)
+// 	}
+
+// 	if value := viper.Get("User"); value != nil {
+// 		User = value.(string)
+// 	}
+
+// 	if value := viper.Get("Server"); value != nil {
+// 		Server = value.(string)
+// 	}
+
+// 	if value := viper.Get("Password"); value != nil {
+// 		Password = value.(string)
+// 	}
+
+// 	if value := viper.Get("Language"); value != nil {
+// 		Language = value.(string)
+// 	}
+
+// 	if value := viper.Get("port"); value != nil {
+// 		Port = value.(string)
+// 	}
+
+// 	if value := viper.Get("uploadPath"); value != nil {
+// 		UploadPath = value.(string)
+// 	}
+// }
--- model.go
+++ model.go
@@ -1,21 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"database/sql"
 	"fmt"
 	"gomachine/config"
 	"os"
-
+	"path"
 	"strings"
 	"time"
+	"unicode"
+	"unicode/utf8"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/CloudyKit/jet/v6"
 	_ "github.com/go-sql-driver/mysql"
-	log "github.com/sirupsen/logrus"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
 )
+
+type Where struct {
+	Column  string
+	Type    string
+	Compare string
+}
+
+type Func struct {
+	Name   string
+	Wheres []Where
+}
 
 
 type Column struct {
@@ -30,7 +43,7 @@ func WriteFile(filename string, content string) error {
 	return os.WriteFile(filename, []byte(content), 0644)
 }
 
-func GetConnection() *sql.DB {
+func GetConnection(config config.ModelConfig) *sql.DB {
 	connectionString := fmt.Sprintf("%v:%v@tcp(%v:3306)/%v", config.User, config.Password, config.Server, config.Database)
 
 	r1, err := sql.Open("mysql", connectionString)
@@ -47,13 +60,51 @@ func GetConnection() *sql.DB {
 }
 
 func main() {
-	path := fmt.Sprintf("%v/bin/buildtool", os.Getenv("HOME"))
+	log.Println("run buildtool model")
+	targetPath := os.Args[1]
 
-	log.Println(path)
-	db := GetConnection()
+	log.Printf("root path : %v\n", targetPath)
+
+	packageName := ""
+
+	if len(os.Args) > 3 {
+		packageName = os.Args[3]
+	} else {
+		file, err := os.Open(path.Join(targetPath, "go.mod"))
+		if err != nil {
+			log.Println("go.mode not found")
+			return
+		}
+		defer file.Close()
+
+		reader := bufio.NewReader(file)
+		for {
+			line, isPrefix, err := reader.ReadLine()
+			if isPrefix || err != nil {
+				break
+			}
+
+			data := strings.Split(string(line), " ")
+			packageName = data[1]
+
+			break
+		}
+	}
+
+	modelConfig := config.Init(targetPath)
+
+	if len(os.Args) > 2 {
+		modelConfig.Language = os.Args[2]
+	}
+
+	log.Println("language :", modelConfig.Language)
+
+	gpas := modelConfig.Gpa
+	
+	db := GetConnection(modelConfig)
 	defer db.Close()
 
-	query := fmt.Sprintf("select table_name from information_schema.tables where table_schema = '%v'", config.Database)
+	query := fmt.Sprintf("select table_name from information_schema.tables where table_schema = '%v'", modelConfig.Database)
 	rows, err := db.Query(query)
 
 	if err != nil {
@@ -70,9 +121,16 @@ func main() {
 		}
 
 		tableName := getTableName(name)
-		println(tableName)
 
-		readColumn(name, db)
+		var gpa *config.Gpa = nil
+		for _, item := range gpas {
+			if item.Name == tableName {
+				gpa = &item
+				break
+			}
+		}
+
+		readColumn(packageName, name, db, gpa, modelConfig.Buildtool, modelConfig.Auth, modelConfig)
 		tables = append(tables, tableName)
 	}
 }
@@ -108,13 +166,15 @@ func getPrefix(name string) string {
 }
 
 
-func readColumn(tableName string, db *sql.DB) {
-	query := "select column_name as column_name, data_type as data_type from information_schema.columns where table_schema = '" + config.Database + "' and table_name = '" + tableName + "'"
+func readColumn(packageName string, tableName string, db *sql.DB, gpa *config.Gpa, version string, auth string, cnf config.ModelConfig) {
+	query := "select column_name as column_name, data_type as data_type from information_schema.columns where table_schema = '" + cnf.Database + "' and table_name = '" + tableName + "'"
 	rows, err := db.Query(query)
 
 	if err != nil {
 		log.Println(err)
 	}
+
+	prefix := ""
 
 	columns := make([]Column, 0)
 	for rows.Next() {
@@ -126,15 +186,75 @@ func readColumn(tableName string, db *sql.DB) {
 			log.Println(err)
 		}
 
-		column := Column{Name: getName(name), Column: name, Type: getType(getName(name), typeid), OriginalType: typeid}
+		column := Column{Name: getName(name), Column: name, Type: getType(getTableName(tableName), getName(name), typeid, gpa, cnf), OriginalType: typeid}
 		columns = append(columns, column)
+
+		prefix = getPrefix(name)
 	}
 
-	process(tableName, columns, db)
+	process(packageName, tableName, prefix, columns, db, gpa, version, auth, cnf)
 }
 
-func getType(name string, t string) string {
-	if config.Language == "dart" || config.Language == "flutter" {
+
+func Split(src string) (entries []string) {
+	// don't split invalid utf8
+	if !utf8.ValidString(src) {
+		return []string{src}
+	}
+	entries = []string{}
+	var runes [][]rune
+	lastClass := 0
+	class := 0
+	// split into fields based on class of unicode character
+	for _, r := range src {
+		switch true {
+		case unicode.IsLower(r):
+			class = 1
+		case unicode.IsUpper(r):
+			class = 2
+		case unicode.IsDigit(r):
+			class = 3
+		default:
+			class = 4
+		}
+		if class == lastClass {
+			runes[len(runes)-1] = append(runes[len(runes)-1], r)
+		} else {
+			runes = append(runes, []rune{r})
+		}
+		lastClass = class
+	}
+	// handle upper case -> lower case sequences, e.g.
+	// "PDFL", "oader" -> "PDF", "Loader"
+	for i := 0; i < len(runes)-1; i++ {
+		if unicode.IsUpper(runes[i][0]) && unicode.IsLower(runes[i+1][0]) {
+			runes[i+1] = append([]rune{runes[i][len(runes[i])-1]}, runes[i+1]...)
+			runes[i] = runes[i][:len(runes[i])-1]
+		}
+	}
+	// construct []string from results
+	for _, s := range runes {
+		if len(s) > 0 {
+			entries = append(entries, string(s))
+		}
+	}
+	return
+}
+
+func getType(tableName string, name string, t string, gpa *config.Gpa, cnf config.ModelConfig) string {
+	if gpa != nil && gpa.Map != nil {
+		for _, item := range gpa.Map {
+			if item.Name == name {
+				if cnf.Language == "dart" || cnf.Language == "flutter" {
+					return strings.Title(name)
+				} else {
+					return tableName + "." + strings.Title(name)
+				}
+			}
+		}
+	}
+
+	if cnf.Language == "dart" || cnf.Language == "flutter" {
 		if t == "int" {
 			return "int"
 		} else if t == "bigint" {
@@ -187,26 +307,330 @@ func getType(name string, t string) string {
 	return t
 }
 
-func process(tableName string, items []Column, db *sql.DB) {
+func tablePrefix(str string, packageName string, db *sql.DB) string {
+	query := "select column_name as column_name, data_type as data_type from information_schema.columns where table_schema = '" + packageName + "' and table_name = '" + str + "_tb'"
+	rows, err := db.Query(query)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	if rows.Next() {
+		var name string
+		var typeid string
+
+		err := rows.Scan(&name, &typeid)
+		if err != nil {
+			log.Println(err)
+		}
+
+		prefix := getPrefix(name)
+		return prefix
+	}
+
+	return ""
+}
+
+func unique(items []Func) []string {
+	ret := make([]string, 0)
+
+	for _, item := range items {
+		for _, where := range item.Wheres {
+			str := where.Type
+			if strings.Index(str, ".") > 0 {
+				strs := strings.Split(str, ".")
+
+				flag := false
+
+				for i := 0; i < len(ret); i++ {
+					if ret[i] == strs[0] {
+						flag = true
+						break
+					}
+				}
+
+				if flag == false {
+					ret = append(ret, strs[0])
+				}
+			}
+		}
+	}
+
+	return ret
+}
+
+func process(packageName string, tableName string, prefix string, items []Column, db *sql.DB, gpa *config.Gpa, version string, auth string, cnf config.ModelConfig) {
 	path := fmt.Sprintf("%v/bin/buildtool", os.Getenv("HOME"))
 
 	var views = jet.NewSet(jet.NewOSFileSystemLoader(path), jet.InDevelopmentMode())
 
 	views.AddGlobal("items", items)
+
+	views.AddGlobal("title", func(str string) string {
+		return strings.Title(str)
+	})
+
+	// views.AddGlobal("untitle", func(str string) string {
+	// 	a := []rune(str)
+	// 	a[0] = unicode.ToLower(a[0])
+	// 	return string(a)
+	// })
+
+	// views.AddGlobal("first", func(str string) string {
+	// 	if str == "" {
+	// 		return ""
+	// 	}
+	// 	ret := strings.Split(str, ":")
+	// 	return ret[0]
+	// })
+
+	// views.AddGlobal("last", func(str string) string {
+	// 	if str == "" {
+	// 		return ""
+	// 	}
+	// 	ret := strings.Split(str, ":")
+	// 	return ret[1]
+	// })
+
+	views.AddGlobal("querytype", func(str string) string {
+		tokens := Split(str)
+
+		return tokens[0]
+	})
+
+	// views.AddGlobal("adjustPackage", func(str string) string {
+	// 	if strings.Index(str, ".") > 0 {
+	// 		return "models." + str
+	// 	} else {
+	// 		return str
+	// 	}
+	// })
+
+	// views.AddGlobal("isNeedImport", func(str string) bool {
+	// 	if strings.Index(str, ".") > 0 {
+	// 		return true
+	// 	} else {
+	// 		return false
+	// 	}
+	// })
+
+	// views.AddGlobal("joinColumn", func(str string, cols []config.GpaJoin) bool {
+	// 	for _, v := range cols {
+	// 		if getName(strings.ToLower(str)) == v.Name {
+	// 			return false
+	// 		}
+	// 	}
+
+	// 	return true
+
+	// })
+
+	views.AddGlobal("compareColumn", func(str string, cols []config.GpaCompare) string {
+		for _, v := range cols {
+			if getName(strings.ToLower(str)) == v.Name {
+				return v.Type
+			}
+		}
+
+		return "="
+
+	})
+
+	// views.AddGlobal("apiurl", func(str string) string {
+	// 	funcName := strings.ToLower(str)
+	// 	url := ""
+	// 	if len(funcName) > 5 && funcName[:5] == "getby" {
+	// 		url = fmt.Sprintf("/get/%v", funcName[5:])
+	// 	} else if len(funcName) > 7 && funcName[:7] == "countby" {
+	// 		url = fmt.Sprintf("/count/%v", funcName[7:])
+	// 	} else if len(funcName) > 6 && funcName[:6] == "findby" {
+	// 		url = fmt.Sprintf("/find/%v", funcName[6:])
+	// 	} else if len(funcName) > 6 && funcName[:6] == "update" {
+	// 		strs := strings.Split(str[6:], "By")
+	// 		url = fmt.Sprintf("/%v/%v", strings.ToLower(strs[0]), strings.ToLower(strs[1]))
+	// 	} else if len(funcName) > 8 && funcName[:8] == "deleteby" {
+	// 		url = fmt.Sprintf("/%v", funcName[8:])
+	// 	} else {
+	// 		url = fmt.Sprintf("/%v", funcName)
+	// 	}
+
+	// 	return url
+	// })
+
+	// views.AddGlobal("columns", func(str string) []Column {
+	// 	query := "select column_name as column_name, data_type as data_type from information_schema.columns where table_schema = '" + packageName + "' and table_name = '" + strings.ToLower(str) + "_tb'"
+	// 	rows, err := db.Query(query)
+
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+
+	// 	columns := make([]Column, 0)
+	// 	for rows.Next() {
+	// 		var name string
+	// 		var typeid string
+
+	// 		err := rows.Scan(&name, &typeid)
+	// 		if err != nil {
+	// 			log.Println(err)
+	// 		}
+
+	// 		prefix := getPrefix(name)
+	// 		column := Column{Name: strings.Title(getName(name)), Column: name, Type: getType(getTableName(str), getName(name), typeid, gpa, cnf), Prefix: prefix}
+	// 		columns = append(columns, column)
+	// 	}
+
+	// 	return columns
+	// })
 	
 	v := make(jet.VarMap)
-
-	v.Set("packageName", config.Database)
+	v.Set("version", version)
+	v.Set("packageName", packageName)
+	v.Set("name", strings.Title(getTableName(tableName)))
 	v.Set("tableName", tableName)
-	v.Set("name", cases.Title(language.Und).String(getTableName(tableName)))
+	v.Set("prefix", prefix)
+	v.Set("items", items)
+	v.Set("auth", auth)
+	if gpa == nil {
+		v.Set("consts", make([]string, 0))
+		v.Set("methods", make([]string, 0))
+		v.Set("funcs", make([]string, 0))
+		v.Set("joins", make([]config.GpaJoin, 0))
+		v.Set("compares", make([]config.GpaCompare, 0))
+		v.Set("sessions", make([]config.SessionPair, 0))
+		v.Set("imports", make([]string, 0))
+	} else {
+		for i := range gpa.Join {
+			gpa.Join[i].Prefix = tablePrefix(gpa.Join[i].Name, packageName, db)
+		}
+		v.Set("consts", gpa.Map)
+		v.Set("methods", gpa.Method)
+		v.Set("joins", gpa.Join)
+		v.Set("compares", gpa.Compare)
+		v.Set("sessions", gpa.Session)
+
+		funcs := make([]Func, 0)
+
+		for _, item := range gpa.Method {
+			tokens := Split(item)
+
+			wheres := make([]Where, 0)
+			if tokens[0] == "Update" {
+				flag := false
+				for i := 1; i < len(tokens); i++ {
+					token := tokens[i]
+					column := ""
+					typename := ""
+					compare := ""
+
+					if token == "By" {
+						flag = true
+						continue
+					} else {
+						for _, name := range items {
+							if token == name.Name {
+								column = name.Name
+								typename = name.Type
+								if flag == true {
+									compare = "where"
+								} else {
+									compare = "column"
+								}
+								break
+							}
+						}
+					}
+
+					where := Where{Column: column, Type: typename, Compare: compare}
+					wheres = append(wheres, where)
+				}
+			} else {
+				for i := 2; i < len(tokens); i++ {
+					token := tokens[i]
+					column := ""
+					typename := ""
+					compare := ""
+					flag := false
+					for _, name := range items {
+						if token == name.Name {
+							column = name.Name
+							typename = name.Type
+							compare = "="
+							flag = true
+							break
+						}
+					}
+
+					if flag == false {
+						for _, name := range items {
+							if token == name.Name+"s" {
+								column = name.Name
+								typename = "[]" + name.Type
+								compare = "in"
+								flag = true
+								break
+							}
+						}
+					}
+
+					if flag == false {
+						for _, name := range items {
+							if token == name.Name+"like" {
+								column = name.Name
+								typename = name.Type
+								compare = "like"
+								flag = true
+								break
+							}
+						}
+					}
+
+					where := Where{Column: column, Type: typename, Compare: compare}
+					wheres = append(wheres, where)
+				}
+			}
+
+			fn := Func{Name: item, Wheres: wheres}
+			funcs = append(funcs, fn)
+		}
+
+		v.Set("funcs", funcs)
+		v.Set("imports", unique(funcs))
+	}
+
+	var b bytes.Buffer
+	modelFilename := "go/model.jet"
+	if cnf.Language == "dart" || cnf.Language == "flutter" {
+		modelFilename = "dart/model.jet"
+	}
+	t, err := views.GetTemplate(modelFilename)
+	if err == nil {
+		if err = t.Execute(&b, v, nil); err != nil {
+			log.Println(err)
+			// error when executing template
+		}
+	} else {
+		log.Println("error ========================")
+		log.Println(err)
+		log.Println("error ========================")
+	}
+
+
+	if cnf.Language == "go" {
+		log.Printf("write file : %v\n", "./models/"+getTableName(tableName)+".go")
+		WriteFile("./models/"+getTableName(tableName)+".go", b.String())
+	}
+
+	v.Set("packageName", packageName)
+	v.Set("tableName", tableName)
+	v.Set("name", strings.Title(getTableName(tableName)))
 	v.Set("partName", getTableName((tableName)))
 	v.Set("consts", make([]string, 0))
 	v.Set("funcs", make([]string, 0))
 
 	var templateBuffers []bytes.Buffer
 
-	modelFilename := "model.jet"
-	if config.Language == "dart" || config.Language == "flutter" {
+	if cnf.Language == "dart" || cnf.Language == "flutter" {
 		arr := []string{"model", "params", "provider", "repository"};
 		for _, a := range arr {
 			modelFilename = "dart/"+a+".jet"
@@ -227,6 +651,41 @@ func process(tableName string, items []Column, db *sql.DB) {
 			filename := fmt.Sprintf("../gym/gym/lib/%s_%s.dart", getTableName(tableName), arr[i])
 			log.Printf("write file: %s\n", filename)
 			WriteFile("../gym/gym/lib/"+arr[i]+"/"+getTableName(tableName)+"_"+arr[i]+".dart", b.String())
+		}
+	} else {
+		var b2 bytes.Buffer
+		t2, err := views.GetTemplate("rest.jet")
+		if err == nil {
+			if err = t2.Execute(&b2, v, nil); err != nil {
+				log.Println(err)
+				// error when executing template
+			}
+		}
+
+		WriteFile("./controllers/rest/"+getTableName(tableName)+".go", b2.String())
+
+		v2 := make(jet.VarMap)
+		v2.Set("version", version)
+		v2.Set("name", strings.Title(getTableName(tableName)))
+		v2.Set("auth", auth)
+		if gpa == nil {
+			v2.Set("consts", make([]string, 0))
+			v2.Set("methods", make([]string, 0))
+			v2.Set("funcs", make([]string, 0))
+		} else {
+			v2.Set("consts", gpa.Map)
+
+			var b2 bytes.Buffer
+			t, err = views.GetTemplate("const.jet")
+			if err == nil {
+				if err = t.Execute(&b2, v2, nil); err != nil {
+					log.Println(err)
+					// error when executing template
+				}
+			}
+
+			os.Mkdir("./models/"+getTableName(tableName), 0755)
+			WriteFile("./models/"+getTableName(tableName)+"/"+getTableName(tableName)+".go", b2.String())
 		}
 	}
 }
